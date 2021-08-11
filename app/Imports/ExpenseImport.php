0099c8c16b475d55c30170aeaa4c8451aeb15b46
Ronak Navadia <ronakn.radicalloop@gmail.com>
2021-08-11 13:44:10 +0530

    fix tags in import expenses

--- app/Imports/ExpenseImport.php
+++ app/Imports/ExpenseImport.php
@@ -3,45 +3,58 @@
 namespace App\Imports;
 
 use App\Expense;
+use App\Models\Medium;
+use App\Tag;
+use Exception;
+use MongoDB\Client;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Support\Facades\DB;
 
 class ExpenseImport implements ToCollection, WithCustomCsvSettings
 {
 
-    //with heading row
     /**
      * @param array $row
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-    // public function model(array $row)
-    // {
-    //     return new Expense([
-    //         'date' => $row['Date'],
-    //         'item' => $row['Item'],
-    //         'amount' => $row['Amount'],
-    //         'medium' => $row['Medium'],
-    //         'tags' => $row['Tags'],
-    //         'notes' => $row['Notes'],
-    //     ]);
-    // }
+
 
     public function collection($rows)
     {
         foreach ($rows as $row) {
-            Expense::create([
+            $medium = Medium::where('medium', $row[3])->first();
+            logger($medium);
+            $tags = explode(";", $row[4]);
+            foreach ($tags as $t) {
+                $getExpenseTag = Tag::select('_id', 'type')->where('tag', $t)->first();
+                if (!$getExpenseTag) {
+                    $tag = new Tag;
+                    $tag->tag = $t;
+                    $tag->type = 'expense';
+                    $tag->save();
+                    $expenseTag[] = $tag->_id;
+                    continue;
+                }
+                if ($getExpenseTag->type == 'expense') {
+                    $expenseTag[] = $getExpenseTag->_id;
+                }
+            }
+
+            $expense = Expense::create([
                 'date' => $row[0],
                 'item' => $row[1],
                 'amount' => $row[2],
-                'medium' => $row[3],
-                'tags' => $row[4],
+                'medium' => ['id' => $medium->_id, 'medium' => $medium->medium],
+                'tag_ids' => $expenseTag,
                 'notes' => $row[5],
             ]);
-            // User::create([
-            //     'name' => $row[0],
-            // ]);
+
+            $expense->tags()->attach($expenseTag);
         }
     }
     public function getCsvSettings(): array
