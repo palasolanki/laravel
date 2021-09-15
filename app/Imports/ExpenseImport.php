f6b40f6e47c3c06931ec5d5a2c36a6328b849e10
Ronak Navadia <ronakn.radicalloop@gmail.com>
2021-09-15 11:40:09 +0530

    fixes in import expense

--- app/Imports/ExpenseImport.php
+++ app/Imports/ExpenseImport.php
@@ -5,51 +5,53 @@ namespace App\Imports;
 use App\Expense;
 use App\Medium;
 use App\Tag;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 
 class ExpenseImport implements ToCollection, WithCustomCsvSettings
 {
-
     /**
      * @param array $row
      *
      * @return \Illuminate\Database\Eloquent\Model|null
      */
-
-
     public function collection($rows)
     {
         $mediumList = Medium::all();
-        foreach ($rows as $row) {
-            $medium = $mediumList->where('medium', $row[3])->first();
-            $tags = explode(";", $row[4]);
-            foreach ($tags as $t) {
-                $getExpenseTag = Tag::select('_id', 'type')->where('tag', $t)->first();
-                if (!$getExpenseTag) {
-                    $tag = new Tag;
-                    $tag->tag = $t;
-                    $tag->type = Tag::TYPE_EXPENSE;
-                    $tag->save();
-                    $expenseTag[] = $tag->_id;
-                    continue;
+        foreach ($rows as $key => $row) {
+            if ($key == 0) {
+                continue;
+            } else {
+                $medium = $mediumList->where('medium', $row[3])->first();
+                $tags   = explode(';', $row[4]);
+                foreach ($tags as $t) {
+                    $getExpenseTag = Tag::select('_id', 'type')->where('tag', $t)->first();
+                    if (!$getExpenseTag) {
+                        $tag       = new Tag;
+                        $tag->tag  = $t;
+                        $tag->type = Tag::TYPE_EXPENSE;
+                        $tag->save();
+                        $expenseTag[] = $tag->_id;
+                        continue;
+                    }
+                    if ($getExpenseTag->type == Tag::TYPE_EXPENSE) {
+                        $expenseTag[] = $getExpenseTag->_id;
+                    }
                 }
-                if ($getExpenseTag->type == Tag::TYPE_EXPENSE) {
-                    $expenseTag[] = $getExpenseTag->_id;
-                }
+                $expense = Expense::create([
+                    'date'   => Carbon::parse($row[0])->format('Y-m-d'),
+                    'item'   => $row[1],
+                    'amount' => $row[2],
+                    'medium' => ['id' => $medium->_id, 'medium' => $medium->medium],
+                    'notes'  => $row[5],
+                ]);
+
+                $expense->tags()->attach($expenseTag);
             }
-
-            $expense = Expense::create([
-                'date' => $row[0],
-                'item' => $row[1],
-                'amount' => $row[2],
-                'medium' => ['id' => $medium->_id, 'medium' => $medium->medium],
-                'notes' => $row[5],
-            ]);
-
-            $expense->tags()->attach($expenseTag);
         }
     }
+
     public function getCsvSettings(): array
     {
         return [
