6f78aa8eefcd11adf2ff3a13ef107e9ffe2f4077
Ronak Navadia <ronakn.radicalloop@gmail.com>
2021-09-16 16:46:56 +0530

    reset errors in edit income

--- resources/js/components/income/Income.js
+++ resources/js/components/income/Income.js
@@ -40,6 +40,7 @@ export default function Income() {
 
     const editRow = income => {
         if (income) {
+            setErrors([]);
             setCurrentIncome(income);
             openShowEdit();
         }
